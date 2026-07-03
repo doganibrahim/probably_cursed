eaa2a9dc2c16ab64835afc2e0077ca79f1cf0cd7
p4n <ibrahimdogan.js@gmail.com>
2026-07-03 23:31:29 +0300

    init: create project folders and main file

---
+++ main.py
@@ -0,0 +1,17 @@
+import pgzrun
+import math
+import random
+
+# oyun penceresi ayarları
+WIDTH = 1000
+HEIGHT = 600
+TITLE = "Probably Cursed - Kodland"
+
+def draw():
+    # şimdilik ekranı geçici bir renkle dolduralım
+    screen.fill((68, 68, 69))
+
+def update():
+    pass
+
+pgzrun.go()
