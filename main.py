49762d941ff5a4503e92ab6a12ac72094206551f
p4n <ibrahimdogan.js@gmail.com>
2026-07-04 00:52:38 +0300

    feat: implement grid system for top-down gameplay

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ WIDTH = 1000
 HEIGHT = 600
 TITLE = "Probably Cursed - Kodland"
 
+TILE_SIZE = 50
+COLS = WIDTH // TILE_SIZE
+ROWS = HEIGHT // TILE_SIZE
+
 # ilk aşamada menu, play ve game_over olacak
 game_state = "menu"
 sound_enabled = True # ses açık mı?
@@ -16,6 +20,23 @@ sound_enabled = True # ses açık mı?
 play_btn = Rect((400, 200), (200, 50))
 sound_btn = Rect((400, 300), (200, 50))
 exit_btn = Rect((400, 400), (200, 50))
+
+def draw_grid():
+    """
+    Oyun alanini esit karelere boler.
+    """
+    for row in range(ROWS):
+        for col in range(COLS):
+            x = col * TILE_SIZE # örn. ilk col. pixel = 50
+            y = row * TILE_SIZE
+
+            # kareleri iki farklı renkle çizerek grid yapısını belirginleştirelim
+            if (row + col) % 2 == 0:
+                color = (50, 50, 55)
+            else:
+                color = (60, 60, 65)
+
+            screen.draw.filled_rect(Rect((x, y), (TILE_SIZE, TILE_SIZE)), color)
 
 def draw():
     # şimdilik ekranı geçici bir renkle dolduralım
@@ -37,6 +58,9 @@ def draw():
 
 
         screen.blit("icons/exit_icon", (exit_btn.x + 80, exit_btn.y))
+
+    elif game_state == "play":
+        draw_grid()
 
 def update():
     pass
